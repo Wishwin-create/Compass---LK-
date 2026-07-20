1d623e4d9dcbf37e603f578816d7e0023eb5c0de
L.L.W.G. Alwis <gesarawishwin@gmail.com>
2026-07-20 20:01:51 +0530

    add the readme file

--- Front_end/destination_data.js
+++ Front_end/destination_data.js
@@ -39,8 +39,8 @@ const DESTINATIONS = {
         title: "Adams Bridge / Rama's Bridge",
         about: "A natural limestone shoal between Sri Lanka and India.",
         topImage: "src/Northern Province/Adams Bridge.jpeg",
-        lat: 8.0817,
-        lng: 81.3420,
+        lat:9.1210,
+        lng: 79.5217,
         gallery: [
             "src/Northern Province/Adams Bridge.jpeg",
             "src/Northern Province/adam_bridge-2.jpg",
@@ -55,8 +55,8 @@ const DESTINATIONS = {
     aluvihare_temple: {
         title: "Aluvihare Temple",
         about: "Aluvihare Temple is a historic Buddhist temple located in Matale, Central Province of Sri Lanka. It is renowned for being the site where the Pali Canon, the sacred scriptures of Theravada Buddhism, was first written down on palm leaves in the 1st century BCE.",
-        lat: 7.4586,
-        lng: 80.5900,
+        lat: 7.50,
+        lng: 80.62,
         topImage: "src/Central Province/aluvihare rock temple.jpg",
         gallery: [
             "src/Central Province/aluvihare rock temple.jpg",
@@ -73,8 +73,8 @@ const DESTINATIONS = {
         title: "Ambuluwawa Tower",
         about: "Ambuluwawa Tower is a unique multi-religious complex located in the Central Province of Sri Lanka. It is known for its distinctive tower that symbolizes unity among different religions and offers panoramic views of the surrounding area.",
         topImage: "src/Central Province/Ambuluwawa Tower.jpg",
-        lat: 7.2906,
-        lng: 80.6333,
+        lat:  7.4972,
+        lng: 80.6219,
         gallery: [
             "src/Central Province/Ambuluwawa Tower.jpg",
             "src/Central Province/Ambuluwawa Tower-2.jpg",
@@ -90,8 +90,8 @@ const DESTINATIONS = {
     anuradhapura_sacred_city: {
         title: "Anuradhapura Sacred City",
         about: "Anuradhapura Sacred City is an ancient city in Sri Lanka that served as the capital of the Anuradhapura Kingdom. It is renowned for its well-preserved ruins of ancient Sri Lankan civilization, including stupas, monasteries, and palaces.",
-        lat: 8.3546,
-        lng: 80.3847,
+        lat:  8.3171,
+        lng: 80.4051,
         topImage: "src/North Central Province/Anuradhapura Sacred City.jpg",
         gallery: [
             "src/North Central Province/Anuradhapura Sacred City.jpg",
@@ -108,8 +108,8 @@ const DESTINATIONS = {
         title: "Angurukaramulla Temple",
         about: "Angurukaramulla Temple is a historic Buddhist temple located in Negombo, Sri Lanka. It is renowned for its impressive architecture, intricate murals, and large Buddha statue, making it a significant religious and cultural site in the region.",
         topImage: "src/Southern Province/Angurukaramulla Temple.jpeg",
-        lat: 6.8096,
-        lng: 80.4994,
+        lat:  7.20966,
+        lng: 79.85376,
         gallery: [
             "src/Southern Province/Angurukaramulla Temple.jpeg",
             "src/Southern Province/Angurukaramulla Temple-2.webp",
@@ -125,8 +125,8 @@ const DESTINATIONS = {
         title: "Arugam Bay",
         about: "Arugam Bay is a popular beach destination located on the east coast of Sri Lanka. Known for its stunning beaches, vibrant surf culture, and laid-back atmosphere, it attracts surfers and beach lovers from around the world.", 
         topImage: "src/Eastern Province/Arugam Bay.jpeg",
-        lat: 6.8402,
-        lng: 81.8309,
+        lat: 6.8589,
+        lng:  81.8304,
         gallery: [
             "src/Eastern Province/Arugam Bay.jpeg",
             "src/Eastern Province/Arugam Bay-2.jpeg",
@@ -143,8 +143,8 @@ const DESTINATIONS = {
         title: "Athugala (Elephant Rock)",
         about: "Athugala, also known as Elephant Rock, is a natural rock formation located in the North Western Province of Sri Lanka. It is named for its resemblance to an elephant and is a popular spot for hiking and sightseeing.",
         topImage: "src/North Western Province/Athugala (Elephant Rock).jpeg",
-        lat: 7.4586,
-        lng: 80.5900,
+        lat: 7.4871,
+        lng:  80.3716,
         gallery: [
             "src/North Western Province/Athugala (Elephant Rock).jpeg",
             "src/North Western Province/Athugala (Elephant Rock)-2.webp",
@@ -161,8 +161,8 @@ const DESTINATIONS = {
         title: "Attidiya Bird Sanctuary",
         about: "Attidiya Bird Sanctuary is a protected area located in the Western Province of Sri Lanka. It is known for its rich biodiversity and serves as a habitat for a variety of bird species, making it a popular destination for birdwatchers and nature enthusiasts.",
         topImage: "src/Western Province/Attidiya Bird Sanctuary.jpg",
-        lat: 7.2906,
-        lng: 80.6333,
+        lat: 6.8439,
+        lng: 79.8857,
         gallery: [
             "src/Western Province/Attidiya Bird Sanctuary.jpg",
             "src/Western Province/Attidiya Bird Sanctuary-2.webp",
@@ -178,8 +178,8 @@ const DESTINATIONS = {
     avukana_buddha_statue: {
         title: "Avukana Buddha Statue",
         about: "The Avukana Buddha Statue is a magnificent ancient statue located in the North Central Province of Sri Lanka. Carved out of a large granite rock face, it stands at approximately 12 meters tall and is renowned for its intricate craftsmanship and serene expression.", 
-        lat: 8.0817,
-        lng: 81.3420,
+        lat: 8.0109,
+        lng: 80.5127,
         topImage: "src/North Central Province/Avukana Buddha Statue.jpeg",  
         gallery: [
             "src/North Central Province/Avukana Buddha Statue.jpeg",
@@ -195,8 +195,8 @@ const DESTINATIONS = {
     balangoda_caves: {
         title: "Balangoda Caves (Fa Hien Cave)",
         about: "Balangoda Caves, also known as Fa Hien Cave, is a significant archaeological site located in the Sabaragamuwa Province of Sri Lanka. It is renowned for its prehistoric cave paintings and artifacts that provide valuable insights into the island's ancient human history.", 
-        lat: 7.4586,
-        lng: 80.5900,
+        lat:  6.6235,
+        lng: 80.8723,
         topImage: "src/Sabaragamuwa Province/Balangoda Caves (Fa Hien Cave nearby).webp",  
         gallery: [  
             "src/Sabaragamuwa Province/Balangoda Caves (Fa Hien Cave nearby).webp",
@@ -212,8 +212,8 @@ const DESTINATIONS = {
     bambarakiri_ella: {
         title: "Bambarakiri Ella",
         about: "Bambarakiri Ella is a scenic waterfall located in the Central Province of Sri Lanka. It is known for its natural beauty and the surrounding lush greenery, making it a popular destination for nature lovers and hikers.",
-        lat: 7.2906,
-        lng: 80.6333,
+        lat:  7.495,
+        lng: 80.699,
         topImage: "src/Central Province/Bambarakiri Ella.jpeg", 
         gallery: [
             "src/Central Province/Bambarakiri Ella.jpeg",
@@ -230,7 +230,7 @@ const DESTINATIONS = {
         title: "Batticaloa Fort",
         about: "Batticaloa Fort is a historical fortification located in the Eastern Province of Sri Lanka. It was built by the Portuguese and later by the Dutch, serving as a strategic military base and now stands as a historical landmark.",
         topImage: "src/Eastern Province/Batticaloa Fort.webp",
-        lat: 7.7100,
+        lat: 7.71667,
         lng: 81.7000,
         gallery: [
             "src/Eastern Province/Batticaloa Fort.webp",
@@ -247,8 +247,8 @@ const DESTINATIONS = {
         title: "Beddagana Wetland Park",
         about: "Beddagana Wetland Park is a protected wetland area located in the Western Province of Sri Lanka. It serves as an important habitat for various bird species and aquatic life, making it a prime destination for birdwatchers and nature enthusiasts.",
         topImage: "src/Western Province/Beddagana Wetland Park.jpg",
-        lat: 6.8096,
-        lng: 80.4994,
+        lat: 6.8905,
+        lng: 79.90874,
         gallery: [
             "src/Western Province/Beddagana Wetland Park.jpg",
             "src/Western Province/Beddagana Wetland Park-2.webp",
@@ -264,8 +264,8 @@ const DESTINATIONS = {
         title: "Ella Rock",
         about: "Ella Rock is a prominent natural rock formation located in the Uva Province of Sri Lanka. Known for its unique shape and scenic views, it offers visitors an opportunity to enjoy panoramic vistas of the surrounding landscape.",
         topImage: "src/Uva Province/Ella Rock.jpeg",
-        lat: 6.8402,
-        lng: 81.8309,
+        lat:6.87528,
+        lng: 81.03833,
         gallery: [
             "src/Uva Province/Ella Rock.jpeg",
             "src/Uva Province/Ella Rock-2.webp",
